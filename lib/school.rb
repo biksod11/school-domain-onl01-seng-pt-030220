cfb54c9a28427a17073defb62b5bc14b3481329e
Bikram Sodhi <biksodhi@gmail.com>
2020-03-17 20:23:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,17 @@ class School
   def add_student(name, grade)
     roster[grade] ||= []
     roster[grade] << name
-
   end
 
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+end
 end
